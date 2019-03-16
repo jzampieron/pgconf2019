eeb12f8fe429be272336bf118485a64864309a06
Jeffrey Zampieron <jzampieron@zproject.net>
2019-03-16 10:14:13 -0400

    Add provisioning to get test files onto the machine

--- terraform/aws-rds-1/vm.tf
+++ terraform/aws-rds-1/vm.tf
@@ -11,34 +11,55 @@
 #
 
 resource "aws_key_pair" "jzpgconf2019" {
-  key_name   = "jzpgconf2019-key"
-  public_key = "${ file( "${path.cwd}/../ssh_keypair.pub" ) }"
+    key_name   = "jzpgconf2019-key"
+    public_key = "${ file( "${path.cwd}/../ssh_keypair.pub" ) }"
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
+    most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 
-  owners = ["099720109477"] # Canonical
+    owners = ["099720109477"] # Canonical
 }
 
 // Standardized on 4 CPU or vCPU machines for this testing.
 resource "aws_instance" "jzpgconf2019" {
-  ami                         = "${data.aws_ami.ubuntu.id}"
-  instance_type               = "m4.xlarge"
-  subnet_id                   = "${aws_subnet.jzpgconf2019.id}"
-  associate_public_ip_address = true
-  key_name                    = "${aws_key_pair.jzpgconf2019.key_name}"
-  vpc_security_group_ids      = [ "${aws_security_group.allow_ssh.id}" ]
+    ami                         = "${data.aws_ami.ubuntu.id}"
+    instance_type               = "m4.xlarge"
+    subnet_id                   = "${aws_subnet.jzpgconf2019.id}"
+    associate_public_ip_address = true
+    key_name                    = "${aws_key_pair.jzpgconf2019.key_name}"
+    vpc_security_group_ids      = [ "${aws_security_group.allow_ssh.id}" ]
+
+    connection {
+        type         = "ssh"
+        host         = "${aws_instance.jzpgconf2019.public_ip}"
+        user         = "ubuntu"
+        timeout      = "10s"
+        private_key  = "${ file( "${path.cwd}/../ssh_keypair" ) }"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mkdir -p /data",
+            "sudo chown ubuntu /data"
+        ]
+    }
+
+    provisioner "file" {
+        source      = "${path.cwd}/../tooling"
+        destination = "/data"
+    }
+
 }
 
 output "JumpBoxPublicIP" {
